a21f7c5df98d5e6d71e85832b7ce3ccfd1758be9
luansilva25 <luansilva250807@gmail.com>
2024-11-29 12:53:03 -0300

    FEAT: Create Natural Sciences API #11

--- config/urls.py
+++ config/urls.py
@@ -7,10 +7,12 @@ router = DefaultRouter()
 from core.authUser.views import UserViewSet
 from core.geminiIA.language.views import LanguageAIViewSet
 from core.geminiIA.mathematics.views import MathAIViewSet
+from core.geminiIA.naturalscience.views import NaturalScienceAIViewSet
 
 router.register("users", UserViewSet)
 router.register("languageAI", LanguageAIViewSet)
 router.register("mathAI", MathAIViewSet)
+router.register("naturalscienceAI", NaturalScienceAIViewSet)
 
 
 urlpatterns = [
---
+++ core/geminiIA/naturalscience/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from core.authUser.models import User
+
+class NaturalScienceAI(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=4000)
+    response = models.TextField(blank=True, null=True)
