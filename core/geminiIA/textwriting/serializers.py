fea9e8188af60aa761494f17beb2e1c5d5906f02
luansilva25 <luansilva250807@gmail.com>
2024-11-30 04:02:53 -0300

    FIX: fixxing user receiver id to email

--- core/geminiIA/textwriting/serializers.py
+++ core/geminiIA/textwriting/serializers.py
@@ -1,7 +1,9 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 from core.geminiIA.textwriting.models import TextWritingAI
+from core.authUser.models import User
 
 class TextWritingAISerializer(ModelSerializer):
+    user = SlugRelatedField(slug_field='email', queryset=User.objects.all())
     class Meta:
         model = TextWritingAI
         fields: list[str] = ['id', 'user', 'answer', 'response', 'cover']
