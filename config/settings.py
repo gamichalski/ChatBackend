6c017480c39e12f3bc4c38346dfc1e5409f2b57d
AnthonyLoche <anthonylocheifc@gmail.com>
2024-11-30 04:41:47 -0300

    ADD FILTER

--- config/settings.py
+++ config/settings.py
@@ -104,6 +104,7 @@ REST_FRAMEWORK = {
     # "DEFAULT_PERMISSION_CLASSES": (
     #     "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     # ),
+    
     }
 
 
--- core/chat/views.py
+++ core/chat/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Chat, Answer
 from .serializers import ChatSerializer, AnswerSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+import django_filters
+from .models import Answer
+
+class AnswerFilter(django_filters.FilterSet):
+    user_id = django_filters.NumberFilter(field_name="chat__user__id")
+
+    class Meta:
+        model = Answer
+        fields = ['user_id']
 
 # Create your views here.
 class ChatViewSet(ModelViewSet):
@@ -11,3 +23,5 @@ class ChatViewSet(ModelViewSet):
 class AnswerViewSet(ModelViewSet):
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AnswerFilter
